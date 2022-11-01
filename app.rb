9a79e1257d3a5133e3a5a4bb2424cec71b6c31a0
Ollie Morley <ollietmorley@gmail.com>
2022-11-01 16:19:47 +0000

    added routes for spaces

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/space_repository'
+require_relative 'lib/database_connection'
+
+DatabaseConnection.connect('valegrobnb_test')
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
@@ -8,5 +12,73 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  #form for listing a new space
+  get '/spaces/new' do
+    return erb(:newspace)
+  end
+
+  #returns a page, listing all spaces. 
+  #Each space links to its individual page with more details/ability to book.
+  get '/spaces' do
+    repo = SpaceRepository.new
+    @spaces = repo.all
+    return erb(:spaces)
+  end
+
+  #returns the page for an individual space, depending on id given
+  get '/spaces/:id' do
+    @id = params[:id]
+
+    repo = SpaceRepository.new
+    @space = repo.find(@id)
+    return erb(:space)
+  end
   
+    post '/spaces/:id' do
+      repo = SpaceRepository.new
+      id = params[:id]
+      repo.delete_space(id)
+      return redirect ('/spaces')
+    end
+  
+  # this route receives params from an erb form and creates a new Space
+  post '/spaces' do
+    repo = SpaceRepository.new
+    @space = Space.new
+
+    @space.name = params[:name]
+    @space.description = params[:description]
+    @space.ppn = params[:ppn] #price_per_night
+    @space.contact = params[:contact]
+    repo.new_space(@space)
+
+    return redirect('/spaces')
+  end
+#this route will update a space
+  get '/update/:id/edit' do
+    repo = SpaceRepository.new
+    id = params[:id]
+    @space = repo.find(id)
+    return erb(:update_space)
+  end
+
+  post '/update/:id' do
+    repo = SpaceRepository.new
+    p id = params[:id]
+    @space = repo.find(id)
+
+    @space.name = params[:name]
+    @space.description = params[:description]
+    @space.ppn = params[:ppn] #price_per_night
+    @space.contact = params[:contact]
+    repo.update_space(@space)
+
+    return redirect('/spaces')
+  end
+  
+#this is route is used for testing
+  get '/test' do 
+    return erb(:test)
+  end
+
 end
---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+require 'rack/test'
+require_relative '../../app'
+
+DatabaseConnection.connect('valegrobnb_test')
+
+describe Application do
+    # This is so we can use rack-test helper methods.
+    include Rack::Test::Methods
+  
+    # We need to declare the `app` value by instantiating the Application
+    # class so our tests work.
+    let(:app) { Application.new }
+
+    context "GET /spaces" do 
+        it "returns a list of all spaces" do 
+        response = get('/spaces')
+
+        expect(response.status).to eq 200
+        expect(response.body).to include('<h1>Choose where you want to stay, or <a href="/spaces/new">list a new space</a></h1>')
+        end 
+    end
+
+    context "GET /spaces/new" do
+        it "returns form to create a new space" do
+            response = get('/spaces/new')
+            expect(response.status).to eq 200
+            expect(response.body).to include('<form action="/spaces" method="POST">')
+        end
+    end
+    
+    context "GET /spaces/:id" do
+        it 'returns details for a space with given id' do
+            response = get('spaces/2')
+            expect(response.status).to eq 200
+            expect(response.body).to include('<h1>windsor castle</h1>')
+        end
+    end
+
+    context "POST /spaces" do
+        it 'creates a new space on homepage' do
+            repo = SpaceRepository.new
+            space = Space.new
+            post('/spaces', name: 'Emmanuel', description:'description for emma', ppn: 30, contact: '0745372838')
+            response = get('/spaces')
+            expect(response.status).to eq 200
+            expect(response.body).to include('Emmanuel')
+        end
+    end
+
+end
